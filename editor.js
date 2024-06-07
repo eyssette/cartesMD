e4704af585742c8f656c93708ca4eac91e75a253
eyssette <cedric.eyssette@gmail.com>
2024-06-07 13:14:12 +0200

    Ajout: editorParam (pour cacher l'éditeur avec ?e=0)

--- editor.js
+++ editor.js
@@ -145,8 +145,10 @@ const toggleEditorElement = document.getElementById("toggleEditor");
 let showEditor = window.innerWidth > 500 ? true : false;
 const params = new URLSearchParams(document.location.search);
 const viewParam = parseInt(params.get("v")) ? parseInt(params.get("v")) : 0;
+const editorParam = params.get("e") == "0" ? 0 : 1;
 const menuParam = params.get("m") == "0" ? 0 : 1;
-if (viewParam == 1) {
+
+if (viewParam == 1 || editorParam == 0) {
 	showEditor = false;
 }
 if (menuParam == 0) {
