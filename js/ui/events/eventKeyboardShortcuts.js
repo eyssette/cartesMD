a5cbd8bf106acb5ba8617e662d7177d2a50d1cbf
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-06-13 14:34:53 +0200

    fix: raccourci clavier pour le mode révision dans flashMD

--- js/ui/events/eventKeyboardShortcuts.js
+++ js/ui/events/eventKeyboardShortcuts.js
@@ -8,25 +8,33 @@ import { orientationButtonElement } from "../menu/menuChangeOrientation";
 export function eventKeyboardShortcuts(editorElement, options) {
 	// Gestion des raccourcis clavier
 	document.body.addEventListener("keydown", (event) => {
-		if ((event.ctrlKey || event.metaKey) && event.key === "p") {
-			event.preventDefault();
-			window.print();
-		}
-		if (event.ctrlKey && event.shiftKey && event.key === "O") {
-			event.preventDefault();
-			orientationButtonElement.click();
-		} else {
-			document.body.classList.remove("hideMenu");
-			if (shouldShowEditor && event.key === "Escape") {
-				hideEditor(editorElement, options);
+		if (!(options && options.isTestModeFromParams)) {
+			if ((event.ctrlKey || event.metaKey) && event.key === "p") {
+				event.preventDefault();
+				window.print();
+			}
+			if (event.altKey && (event.key === "®" || event.key === "r")) {
+				event.preventDefault();
+				const testModeButton = document.querySelector("#testModeButton");
+				testModeButton.click();
+			}
+
+			if (event.ctrlKey && event.shiftKey && event.key === "O") {
+				event.preventDefault();
+				orientationButtonElement.click();
 			} else {
-				if (!shouldShowEditor && event.key === "e") {
-					event.preventDefault();
-					showEditor(editorElement, options);
-					editorElement.focus();
+				document.body.classList.remove("hideMenu");
+				if (shouldShowEditor && event.key === "Escape") {
+					hideEditor(editorElement, options);
 				} else {
-					if (!shouldShowEditor && event.key === "m") {
-						document.body.classList.add("hideMenu");
+					if (!shouldShowEditor && event.key === "e") {
+						event.preventDefault();
+						showEditor(editorElement, options);
+						editorElement.focus();
+					} else {
+						if (!shouldShowEditor && event.key === "m") {
+							document.body.classList.add("hideMenu");
+						}
 					}
 				}
 			}
