642ed1998253ec6a277ee0b0bd84d25162e6b26a
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-06-15 03:06:05 +0200

    chore: configuration rollup et taskfile

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,12 +3,25 @@ import { string } from "rollup-plugin-string";
 import postcss from "rollup-plugin-postcss";
 import cssnano from "cssnano";
 
+const ECMA_VERSION = 2018;
+const optionsTerser = { ecma: ECMA_VERSION };
+
+// On supprime certains messages d'erreurs qu'affiche Rollup et qui ne sont pas très utiles
+const onwarn = (warning) => {
+	if (warning.code === "THIS_IS_UNDEFINED") {
+		// On désactive le message d'erreur affiché à cause du module showdown
+		return;
+	}
+	console.warn(`(!) ${warning.message}`);
+};
+
 export default {
 	input: "js/main.js",
+	onwarn,
 	output: {
 		file: "script.min.js",
 		format: "iife",
-		plugins: [terser()],
+		plugins: [terser(optionsTerser)],
 		sourcemap: true,
 	},
 	plugins: [
