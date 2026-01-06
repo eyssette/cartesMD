8c7621ab2e3c979ab9a146cc57309bad402afb96
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2026-01-06 19:15:24 +0100

    fix(css): impression recto/verso - prise en compte élément "aside" dans le verso

--- js/utils/css.js
+++ js/utils/css.js
@@ -5,11 +5,19 @@ export function isValidColorNameCSS(string) {
 }
 
 // ajoute un scope devant chaque règle CSS définie dans la variable styles
+// sauf pour certaines règles spéciales (media queries, keyframes …)
+const noScopePatterns = ["@media", "@keyframes", "from", "to", "font-face"];
 export function scopedStyles(styles, scope) {
 	let scopedStyles = styles.replace(/([^{}]+)\s*\{/g, (match, selectors) => {
 		let scopedSelectors = selectors
 			.split(",")
-			.map((selector) => `${scope} ${selector.trim()}`)
+			.map((selector) => {
+				if (noScopePatterns.some((pattern) => selector.includes(pattern))) {
+					return selector.trim();
+				} else {
+					return `${scope} ${selector.trim()}`;
+				}
+			})
 			.join(", ");
 		return `${scopedSelectors} {`;
 	});
